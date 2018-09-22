55c4d897451d34b3aeac692a68eb038ac3dbb8a4
Isaias Valdez <isaias.arr.valdez@gmail.com>
2018-09-21 23:30:21 -0500

    Added comments to default style

--- canvas/icon_styles/default.js
+++ canvas/icon_styles/default.js
@@ -24,28 +24,31 @@ module.exports = {
         ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
        
         ctx.font = applyText(canvas, wins);
-
+        // calculate horizontal center position
+        const x_position = canvas.width / 2;
+        // calculate vertical position using M as the baseline because that works for some reason????
+        const y_position = (canvas.height / 2) + (ctx.measureText('M').width / 2);
         ctx.textAlign = 'center';
-
         ctx.shadowColor = 'black';
         ctx.shadowBlur = 32;
 
+        // stroke text
         ctx.strokeStyle = 'black';
         ctx.lineWidth = 6;
-        ctx.strokeText(wins, canvas.width / 2, (canvas.height / 2) + (ctx.measureText('M').width / 2));
+        ctx.strokeText(wins, x_position, y_position);
         
-        // Create gradient
+        // create gradient
         const grd = ctx.createLinearGradient(150.000, 300.000, 150.000, 0.000);
 
-        // Add colors
+        // add colors
         grd.addColorStop(0.300, 'rgba(231, 127, 17, 1.000)');
         grd.addColorStop(0.460, 'rgba(254, 221, 30, 1.000)');
         grd.addColorStop(0.497, 'rgba(254, 221, 30, 1.000)');
         grd.addColorStop(0.800, 'rgba(254, 191, 65, 1.000)');
         
-        // Fill with gradient
+        // fill with gradient
         ctx.fillStyle = grd;
-        ctx.fillText(wins, canvas.width / 2, (canvas.height / 2) + (ctx.measureText('M').width / 2));
+        ctx.fillText(wins, x_position, y_position);
 
         return canvas;
     },
